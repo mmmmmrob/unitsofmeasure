662b019732282982ec7a762d15e9c21cab6f6990
mmmmmrob <rob.styles@dynamicorange.com>
2012-01-05 14:59:22 +0000

    added json and html views

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
 find . -name *.ttl | sed -e "s%\(.*\)\.ttl%rapper -i turtle -o rdfxml-abbrev \1.ttl > \1.rdf%" | bash
+
+find . -name *.ttl | sed -e "s%\(.*\)\.ttl%rapper -i turtle -o json \1.ttl > \1.json%" | bash
+
+find . -name *.ttl | sed -e "s%\(.*\)\.ttl%rapper -i turtle -o html \1.ttl > \1.html%" | bash
+
